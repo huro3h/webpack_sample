f681e69107b0fc884534cbd3e165fad60b6a2afd
huro3h <huro3h@gmail.com>
2021-08-10 20:30:34 +0900

    css-loader, style-loader settings to webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import { APP_NAME, hello, Figure } from "./myutil";
+import './style.css'
+
+let body = document.getElementsByTagName('body')[0];
+body.classList.add('sky');
 
 console.log(APP_NAME);
-hello()
-console.log(Figure.double(250));
-
-let environment_text = ''
-
-if (process.env.NODE_ENV === 'development') {
-  environment_text = 'development!';
-} else if (process.env.NODE_ENV === 'production') {
-  environment_text = 'production?';
-} else {
-  environment_text = 'wakaran..';
-}
-
-console.log(environment_text);
+// hello()
+// console.log(Figure.double(250));
+//
+// let environment_text = ''
+//
+// if (process.env.NODE_ENV === 'development') {
+//   environment_text = 'development!';
+// } else if (process.env.NODE_ENV === 'production') {
+//   environment_text = 'production?';
+// } else {
+//   environment_text = 'wakaran..';
+// }
+//
+// console.log(environment_text);
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
   // 開発環境でのデバッグ用, minifyではなくオリジナルに近いコードがマッピングされる
   // https://webpack.js.org/configuration/devtool/
   devtool:'eval-source-map',
+  module: {
+    rules: [
+      {
+        // css-loader, style-loaderの有効化
+        test: /\.css$/,
+        use: [
+          // ローダーは指定された逆順で処理される
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  
   output: {
     // 出力先フォルダ
     path: `${__dirname}/dist`,
