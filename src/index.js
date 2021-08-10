3acaf80ee7eeb6c2ff5424f09f5ba46604b7f9af
huro3h <huro3h@gmail.com>
2021-08-10 21:57:13 +0900

    update files for sample image

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { APP_NAME, hello, Figure } from "./myutil";
 import './style.css'
+import picture from './images/bird.jpg'
 
-let body = document.getElementsByTagName('body')[0];
-body.classList.add('sky');
-
-console.log(APP_NAME);
+window.addEventListener('DOMContentLoaded', function(){
+  let img = new Image();
+  
+  img.src = picture;
+  document.body.appendChild(img);
+}, false);
