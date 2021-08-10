cc33e11011e25b0a74049e28e8a418fa90710c7c
huro3h <huro3h@gmail.com>
2021-08-10 18:08:39 +0900

    add files

---
+++ myutil.js
@@ -0,0 +1,14 @@
+let message = 'nemui?'
+
+export const APP_NAME = 'webpack_sample'
+
+export function hello() {
+  console.log(message);
+}
+
+// Class
+export class Figure {
+  static double(base) {
+    return base * 2
+  }
+}
