2a0ebffe4815cf889f01a3e4d9ea2972da76b5f5
huro3h <huro3h@gmail.com>
2021-08-10 20:04:33 +0900

    debug

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import { APP_NAME, hello, Figure } from "./myutil";
 console.log(APP_NAME);
 hello()
 console.log(Figure.double(250));
+
+let environment_text = ''
+
+if (process.env.NODE_ENV === 'development') {
+  environment_text = 'development!';
+} else if (process.env.NODE_ENV === 'production') {
+  environment_text = 'production?';
+} else {
+  environment_text = 'wakaran..';
+}
+
+console.log(environment_text);
