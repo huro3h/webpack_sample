b46265722e53c8b07a8ddf8904dbb99daa2eaddd
huro3h <huro3h@gmail.com>
2021-08-10 20:43:20 +0900

    remove unused code

--- src/index.js
+++ src/index.js
@@ -5,17 +5,3 @@ let body = document.getElementsByTagName('body')[0];
 body.classList.add('sky');
 
 console.log(APP_NAME);
-// hello()
-// console.log(Figure.double(250));
-//
-// let environment_text = ''
-//
-// if (process.env.NODE_ENV === 'development') {
-//   environment_text = 'development!';
-// } else if (process.env.NODE_ENV === 'production') {
-//   environment_text = 'production?';
-// } else {
-//   environment_text = 'wakaran..';
-// }
-//
-// console.log(environment_text);
