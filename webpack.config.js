40a0d6e04bb0a76caa876797adff84685ec585b5
huro3h <huro3h@gmail.com>
2021-08-10 19:53:10 +0900

    add development environment tool settings

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ module.exports = {
   mode: 'development',
   // エントリーポイント
   entry: './src/index.js',
+  // 開発環境でのデバッグ用, minifyではなくオリジナルに近いコードがマッピングされる
+  // https://webpack.js.org/configuration/devtool/
+  devtool:'eval-source-map',
   output: {
     // 出力先フォルダ
     path: `${__dirname}/dist`,
