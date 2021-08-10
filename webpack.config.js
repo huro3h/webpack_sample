4f01a7359eb900ee97688a55de39faf950d77587
huro3h <huro3h@gmail.com>
2021-08-10 18:32:46 +0900

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'development',
+  // エントリーポイント
+  entry: './src/index.js',
+  output: {
+    // 出力先フォルダ
+    path: `${__dirname}/dist`,
+    // 出力先ファイル名
+    filename: "main.js"
+  }
+}
