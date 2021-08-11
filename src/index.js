40ca227f2d58125ba4f1ecaab23c7cd1d05968d1
huro3h <huro3h@gmail.com>
2021-08-11 09:06:20 +0900

    sample.json import as jsonData

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { APP_NAME, hello, Figure } from "./myutil";
 import './style.css'
 import picture from './images/bird.jpg'
+import jsonData from './sample.json'
 
 // window.addEventListener('DOMContentLoaded', function(){
 //   let img = new Image();
@@ -8,3 +9,7 @@ import picture from './images/bird.jpg'
 //   img.src = picture;
 //   document.body.appendChild(img);
 // }, false);
+
+console.log(jsonData[0].id);
+console.log(jsonData[0].title);
+console.log(jsonData[0].quantity);
